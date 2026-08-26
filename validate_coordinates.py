3b4057d304ba8b4d732d4e942c9b8880365e3983
Parang9423 <parang5736@gmail.com>
2026-08-26 15:51:42 +0900

    Add coordinate validation CLI entrypoint

---
+++ validate_coordinates.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""CLI entrypoint for AOI/ODB coordinate validation."""
+from aoi.coordinate_validation import main
+
+if __name__ == "__main__":
+    raise SystemExit(main())
