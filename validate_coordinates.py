bdedbc86f854e9eb367b25c4822560b5c935bf18
Parang9423 <parang5736@gmail.com>
2026-09-08 10:54:15 +0900

    Remove obsolete initial coordinate validation CLI

--- validate_coordinates.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-"""CLI entrypoint for AOI/ODB coordinate validation."""
-from aoi.coordinate_validation import main
-
-if __name__ == "__main__":
-    raise SystemExit(main())
